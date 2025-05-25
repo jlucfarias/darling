f447dc7943d5b038ccce5ed9edd492de0d87e9f2
jlucfarias <jlalmeida.farias@gmail.com>
2025-05-25 11:31:22 -0300

    Add symbols for SubEthaEdit app

---
+++ src/frameworks/WebKit/src/WebView.m
@@ -0,0 +1,6 @@
+#import <WebKit/WebView.h>
+
+NSString * WebElementImageKey = @"WebElementImage";
+NSString * WebElementLinkURLKey = @"WebElementLinkURL";
+NSString * WebElementLinkTargetFrameKey = @"WebElementLinkTargetFrame";
+NSString * WebElementLinkTitleKey = @"WebElementLinkTitle";
